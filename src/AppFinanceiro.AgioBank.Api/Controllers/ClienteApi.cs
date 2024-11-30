659e90a10b507f730303902e821d39763ac40dcc
Rapha2790 <rssshev@gmail.com>
2024-11-30 15:27:45 -0300

    Adicionado tipagem no endpoint

---
+++ src/AppFinanceiro.AgioBank.Api/Controllers/ClienteApi.cs
@@ -0,0 +1,28 @@
+﻿using AppFinanceiro.AgioBank.Application.UseCases.Cliente.Inativar.Requests;
+using AppFinanceiro.AgioBank.Application.UseCases.Common.Responses;
+using MediatR;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppFinanceiro.AgioBank.Api.Controllers;
+
+public static class ClienteApi
+{
+    public static void MapClientRoutes(this IEndpointRouteBuilder app)
+    {
+        var clientGroup = app.MapGroup("api/clientes")
+            .WithTags("Clientes");
+        
+        
+        clientGroup.MapDelete("inativar/{id:guid}", async Task<Results<NoContent, BadRequest<Resultado>>> (Guid id, [FromServices] IMediator mediator, [FromBody] InativarClienteRequest request) =>
+        {
+            request.Id = id;
+            var response = await mediator.Send(request);
+            
+            if(response.Sucesso)
+                return TypedResults.NoContent();
+            
+            return TypedResults.BadRequest(response);
+        });
+    }
+}
